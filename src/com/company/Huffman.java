c92cef5699638f9df246d0c65fdb82c3ef87d973
ArShAm <cloner10001@gmail.com>
2021-01-14 21:09:28 +0330

    Add Main function

--- src/com/company/Huffman.java
+++ src/com/company/Huffman.java
@@ -44,4 +44,11 @@ public class Huffman {
             e.printStackTrace();
         }
     }
+    //Main function
+    public void function() {
+        if (compression)
+            compression();
+        else
+            decompression();
+    }
 }
