8d33d803a80cabeb77589109ccce768cbabe477b
ArShAm <cloner10001@gmail.com>
2021-01-14 21:08:06 +0330

    Add compression

--- src/com/company/Huffman.java
+++ src/com/company/Huffman.java
@@ -1,11 +1,35 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Huffman {
     private final String address;
     private final boolean compression;
 
+    //Constructor
     public Huffman(String address) {
         this.address = address;
         compression = address.endsWith("txt");
+    }
+    // Read and run Class Compression
+    private void compression() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(address));
+            StringBuilder data = new StringBuilder();
+            while (bufferedReader.ready()) {
+                data.append(bufferedReader.readLine());
+                data.append("\n");
+            }
+            bufferedReader.close();
+            Compression compress = new Compression(data.toString());
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(address.substring(0, address.length() - 3) + "cmp"));
+            bufferedWriter.write(compress.compress());
+            bufferedWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
