5ca2384757891e30e1d7737aab149520bdb813b9
ArShAm <cloner10001@gmail.com>
2021-01-14 20:48:00 +0330

    Add Constructor

--- src/com/company/Compression.java
+++ src/com/company/Compression.java
@@ -1,5 +1,16 @@
 package com.company;
 
-public class Compression {
+import java.util.HashMap;
 
+public class Compression {
+    private final String words;
+    private final HashMap<Character, String> charData;
+    private final HashMap<Character, Integer> charCount;
+    private HuffmanNode root;
+
+    public Compression(String words) {
+        this.words = words;
+        charData = new HashMap<>();
+        charCount = new HashMap<>();
+    }
 }
