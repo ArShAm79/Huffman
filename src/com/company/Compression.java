2c9ca6e3c5866b984cf77374bbe08ede43fb55e8
ArShAm <cloner10001@gmail.com>
2021-01-24 19:13:33 +0330

    Delete setRoot and setCharData from Compression

--- src/com/company/Compression.java
+++ src/com/company/Compression.java
@@ -38,45 +38,6 @@ public class Compression {
         }
     }
 
-    //Build tree
-    private void setRoot() {
-        PriorityQueue<HuffmanNode> queue = new PriorityQueue<>(charCount.size(), new ImplementComparator());
-        for (char c : charCount.keySet()) {
-            HuffmanNode huffmanNode = new HuffmanNode();
-            huffmanNode.c = c;
-            huffmanNode.item = charCount.get(c);
-            huffmanNode.left = null;
-            huffmanNode.right = null;
-            queue.add(huffmanNode);
-        }
-        root = null;
-        while (queue.size() > 1) {
-            HuffmanNode left = queue.peek();
-            queue.poll();
-            HuffmanNode right = queue.peek();
-            queue.poll();
-            HuffmanNode huffmanNode = new HuffmanNode();
-            assert right != null;
-            huffmanNode.item = left.item + right.item;
-            huffmanNode.c = '-';
-            huffmanNode.left = left;
-            huffmanNode.right = right;
-            root = huffmanNode;
-            queue.add(huffmanNode);
-        }
-    }
-
-    //Build binary code for chars
-    private void setCharData(HuffmanNode root, String value) {
-        if (root != null && root.left == null && root.right == null) {
-            charData.put(root.c, value);
-            return;
-        } else if (root == null)
-            return;
-        setCharData(root.left, value + "0");
-        setCharData(root.right, value + "1");
-    }
-
     //Convert binary code to char
     private char stringToChar(String str) {
         int ans = 0;
